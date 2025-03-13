bb6529551d8f425fbd45664e57b7c8dddddd7e6a
Manohara91 <manohara.hc91@gmail.com>
2025-03-13 13:23:05 +0530

    prod url

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    production : false,
-    apiUrl : 'http://localhost:5000/api'  // Development API URL
+    production : true,
+    // apiUrl : 'http://localhost:5000/api'  // Development API URL
+    apiUrl: 'https://online-appointment-booking-system-nodejs.onrender.com/api'
 }
